420c6e5487981f197c74858a48b83c60a9818f56
Ilanga <Ilanga@git.com>
2023-06-15 00:36:27 +0600

    Add schedule design

--- massage_clients/clients_data/urls.py
+++ massage_clients/clients_data/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CreateClientView.as_view(), name='client_form'),  # URL for the first page with the form to fill
-    path('clients-list/', views.AllClientsView.as_view(), name='clients_list'),  # URL for the second page with the actual clients list
+    path('', views.CreateClientView.as_view(), name='client_form'),
+    path('clients-list/', views.AllClientsView.as_view(), name='clients_list'),
     path('clients/<int:pk>/<str:name>/', views.SingleClientDisplayView.as_view(), name='single_client'),
     path('clients/<int:pk>/<str:name>/visits/edit', views.ClientVisitsEditView.as_view(), name='client_visits_edit'),
     path('clients/<int:pk>/<str:name>/client/edit', views.SingleClientUpdateView.as_view(), name='client_update'),
     path('schedule/', views.TimetableView.as_view(), name='schedule'),
-    # path('schedule/', views.schedule, name='schedule'),  # URL for the fourth page with the schedule
 ]
