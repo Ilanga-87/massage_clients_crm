5a07e91d0c03532b0a6e1156c22f185fdec1f650
Ilanga <Ilanga@git.com>
2023-06-28 18:51:08 +0600

    Change design of Schedule table

--- massage_clients/clients_data/views.py
+++ massage_clients/clients_data/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponseRedirect
 from django.views.generic import (TemplateView, ListView, CreateView, DetailView, FormView, UpdateView)
 from django.views.generic.detail import SingleObjectMixin
 from django.urls import reverse
-from django.db.models import Min, F, Q
+from django.db.models import Min, F, Q, Value, ExpressionWrapper, DateTimeField, CharField
+from django.db.models.functions import Concat, Cast
 
 from .forms import ClientForm, VisitFormSet
 from .models import Client, Visit
@@ -24,23 +25,56 @@ class AllClientsView(ListView):
     model = Client
     template_name = 'clients_data/clients_list.html'  # Specify the template name explicitly
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.GET.get('search')
+
+        if search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=search_query) |
+                Q(phone_number__icontains=search_query) |
+                Q(visit_client__visit_date__icontains=search_query) |
+                Q(visit_client__visit_time__icontains=search_query) |
+                Q(age__icontains=search_query)
+            ).distinct()
+
+        return queryset
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        ordering = self.request.GET.get('ordering')  # Get the ordering parameter from the request
 
+        closest_visits = (
+            Visit.objects
+            .annotate(closest_visit_datetime=ExpressionWrapper(Concat(
+                F('visit_date'), Value(' '), F('visit_time'),
+            ),
+                output_field=DateTimeField()))
+            .values('client_id')
+            .annotate(closest_visit=Min('closest_visit_datetime'))
+        )
+
+        closest_visit_dict = {
+            visit['client_id']: visit['closest_visit']
+            for visit in closest_visits}
+        context['closest_visits'] = closest_visit_dict
+
+        ordering = self.request.GET.get('ordering')  # Get the ordering parameter from the request
         if ordering == 'name':
             context['object_list'] = self.model.objects.order_by('name')
         elif ordering == 'closest_visit':
             closest_visits = (
                 Visit.objects
+                .annotate(closest_visit_datetime=Cast(
+                    Concat(F('visit_date'), Value(' '), Cast(F('visit_time'), output_field=CharField())),
+                    output_field=DateTimeField()
+                ))
                 .values('client_id')
-                .annotate(closest_visit=Min('visit_date'))
+                .annotate(closest_visit=Min('closest_visit_datetime'))
+                .order_by('closest_visit')
             )
-            closest_visit_dict = {visit['client_id']: visit['closest_visit'] for visit in closest_visits}
-            context['object_list'] = self.model.objects.annotate(
-                closest_visit_date=Min('visit_client__visit_date')
-            ).order_by(F('closest_visit_date').asc(nulls_last=True))
-            context['closest_visits'] = closest_visit_dict
+
+            client_ids = [visit['client_id'] for visit in closest_visits]
+            context['object_list'] = self.model.objects.filter(pk__in=client_ids)
 
         return context
 
@@ -122,12 +156,12 @@ class TimetableView(TemplateView):
             for j in range(len(dates)):
                 if time_range[i] != time_range[-1]:
                     visit = Visit.objects.filter(visit_date=dates[j], visit_time__gte=time_range[i],
-                                                 visit_time__lt=time_range[i+1])
+                                                 visit_time__lt=time_range[i + 1])
                 else:
                     visit = Visit.objects.filter(visit_date=dates[j], visit_time__gte=time_range[i])
                 if len(visit) > 0:
                     for item in visit:
-                        row[j+1] = item
+                        row[j + 1] = item
             timetable_matrix.append(row)
 
         context['dates'] = dates
@@ -143,4 +177,33 @@ class CompletedVisitsListView(ListView):
     context_object_name = 'completed_visits'
 
     def get_queryset(self):
-        return self.model.objects.filter(done_and_paid=True)
+        queryset = super().get_queryset()
+        completed_visits = self.model.objects.filter(done_and_paid=True)
+
+        search_query = self.request.GET.get('search')
+
+        if search_query:
+            completed_visits = completed_visits.filter(
+                Q(client__name__icontains=search_query) |
+                Q(massage_type__icontains=search_query) |
+                Q(visit_date__icontains=search_query) |
+                Q(client__age__icontains=search_query) |
+                Q(visit_time__icontains=search_query) |
+                Q(visit_price__icontains=search_query)
+            ).distinct()
+        return completed_visits
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        ordering = self.request.GET.get('ordering')  # Get the ordering parameter from the request
+
+        if ordering == 'visit_date':
+            context['object_list'] = self.model.objects.filter(done_and_paid=True).order_by('visit_date')
+        elif ordering == 'massage_type':
+            context['object_list'] = self.model.objects.filter(done_and_paid=True).order_by('massage_type')
+        elif ordering == 'visit_price':
+            context['object_list'] = self.model.objects.filter(done_and_paid=True).order_by('visit_price')
+        elif ordering == 'name':
+            context['object_list'] = self.model.objects.filter(done_and_paid=True).order_by('client__name')
+
+        return context
